fb8d26e4673598d36ad175474a0a3932d9861107
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-16 12:35:15 -0600

    Add IUser interface

---
+++ src/ts/interfaces/user.ts
@@ -0,0 +1,30 @@
+// Third-party dependencies
+import { Schema } from "mongoose"
+
+// Current project dependencies
+
+/**
+ * Interface representing an user.
+ */
+export interface IUser {
+  username: string
+  email: string
+  password: string
+  firstname: string
+  lastname: string
+  description?: string
+  avatarUrl?: string
+  coverUrl?: string
+  website?: string
+  posts: Schema.Types.ObjectId[]
+  friends: Schema.Types.ObjectId[]
+  friendRequests: Schema.Types.ObjectId[]
+  friendRequestsSent: Schema.Types.ObjectId[]
+  facebookLink?: string
+  githubLink?: string
+  linkedinLink?: string
+  twitterLink?: string
+  instagramLink?: string
+  createdAt: Date
+  updatedAt: Date
+}
