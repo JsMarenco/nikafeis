23451a7e0a62967b4b3ce5517ac552f7e58815ce
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-16 16:08:17 -0600

    Add username generation function

--- src/utils/basic/index.ts
+++ src/utils/basic/index.ts
@@ -1,4 +1,5 @@
 // Third-party dependencies
+import { v4 as uuid } from "uuid"
 
 // Current project dependencies
 
@@ -20,4 +21,18 @@ export const validateSimpleEmail = (email: string): boolean => {
   const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
 
   return re.test(email)
+}
+
+/**
+ * Generates a username by combining a prefix with a unique identifier.
+ * @returns {string} - The generated username.
+ *
+ * @example
+ * const generatedUsername = generateUsername();
+ * console.log(generatedUsername); // Output: "user-123e4567-e89b"
+ */
+export const generateUsername = (): string => {
+  const username = `user-${uuid()}`
+
+  return username.substring(0, 15)
 }
