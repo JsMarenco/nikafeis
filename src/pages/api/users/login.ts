b15ea01e819a9cf7b5e683a36cc9a8edadb8f39a
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-16 16:55:46 -0600

    Add login endpoint

---
+++ src/pages/api/users/login.ts
@@ -0,0 +1,19 @@
+// Third-party dependencies
+import type { NextApiRequest, NextApiResponse } from "next"
+
+// Current project dependencies
+import httpStatus from "@/constants/common/httpStatus"
+import loginUser from "@/controllers/user/loginUser"
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  switch (req.method) {
+    case "POST":
+      loginUser(req, res)
+      break
+
+    default:
+      res.status(httpStatus.methodNotAllowed.code).json({
+        message: httpStatus.methodNotAllowed.message,
+      })
+  }
+}
