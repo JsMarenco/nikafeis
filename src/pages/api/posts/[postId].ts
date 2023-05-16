54093a44a6cae4f030b0315d72aa340037005a43
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-16 13:33:15 -0600

    Add deletePost controller to the endpoint

--- src/pages/api/posts/[postId].ts
+++ src/pages/api/posts/[postId].ts
@@ -4,11 +4,16 @@ import type { NextApiRequest, NextApiResponse } from "next"
 // Current project dependencies
 import httpStatus from "@/constants/common/httpStatus"
 import getPost from "@/controllers/post/getPost"
+import deletePost from "@/controllers/post/deletePost"
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   switch (req.method) {
     case "GET":
       getPost(req, res)
+      break
+
+    case "DELETE":
+      deletePost(req, res)
       break
 
     default:
