67b4e8eb6f0f5632fb9f8f6e77e87c9affb580ce
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-17 13:56:04 -0600

    Add like post service

---
+++ src/services/post/likePostService.ts
@@ -0,0 +1,52 @@
+// Third-party dependencies
+import axios from "axios"
+
+// Current project dependencies
+import httpStatus from "@/constants/common/httpStatus"
+import { IGlobalApiResponse } from "@/ts/interfaces/api"
+import apiRoutes from "@/constants/api/routes"
+
+const likePostService = async (
+  userId: string,
+  token: string,
+  postId: string
+) => {
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+
+    const { data, status } = await axios.post(
+      apiRoutes.post.like(postId, userId),
+      config
+    )
+
+    const newResponse: IGlobalApiResponse = {
+      body: data,
+      message: data.message,
+      status: status,
+      success: true,
+    }
+
+    return newResponse
+  } catch (err: unknown) {
+    const apiAnswer = {
+      body: [],
+      message: httpStatus.badRequest.message,
+      success: false,
+      status: httpStatus.badRequest.code,
+    }
+
+    if (axios.isAxiosError(err)) {
+      apiAnswer.body = err.response?.data
+      apiAnswer.message = err.response?.data.message
+      apiAnswer.status = err.response?.status || httpStatus.badRequest.code
+    }
+
+    return apiAnswer
+  }
+}
+
+export default likePostService
