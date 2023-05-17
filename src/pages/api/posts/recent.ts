6e145590de9e254af27b36abfbd0127b66310c1f
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-17 12:15:19 -0600

    Add endpoint for retrieving recent posts

---
+++ src/pages/api/posts/recent.ts
@@ -0,0 +1,19 @@
+// Third-party dependencies
+import type { NextApiRequest, NextApiResponse } from "next"
+
+// Current project dependencies
+import httpStatus from "@/constants/common/httpStatus"
+import getRecentPosts from "@/controllers/post/getRecentPosts"
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  switch (req.method) {
+    case "GET":
+      getRecentPosts(req, res)
+      break
+
+    default:
+      res.status(httpStatus.methodNotAllowed.code).json({
+        message: httpStatus.methodNotAllowed.message,
+      })
+  }
+}
