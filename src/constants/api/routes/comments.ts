8e5202516fa6870c30e38c0c8f67d2a699db4a84
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-17 16:21:06 -0600

    Minor changes

--- src/constants/api/routes/comments.ts
+++ src/constants/api/routes/comments.ts
@@ -2,13 +2,20 @@ const baseUrl = "/api/comments"
 
 const apiCommentsRoutes = {
   create: `${baseUrl}`,
+
   update: (commentId: string, userId: string) =>
     `${baseUrl}/${commentId}?userId=${userId}`,
+
   like: (commentId: string, userId: string) =>
     `${baseUrl}/${commentId}/like?userId=${userId}`,
+
   delete: (commentId: string, userId: string) =>
     `${baseUrl}/${commentId}?userId=${userId}`,
-  get: (commentId: string) => `${baseUrl}/${commentId}`,
+
+  getById: (commentId: string) => `${baseUrl}/${commentId}`,
+
+  getComments: (offset: number, limit: number) =>
+    `${baseUrl}/list?offset=${offset}&limit=${limit}`,
 }
 
 export default apiCommentsRoutes
