64eb27926015ecd8bc971cafdce535bdc4e31286
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-17 13:55:39 -0600

    Add get recent posts service

---
+++ src/services/post/getRecentPostsService.ts
@@ -0,0 +1,41 @@
+// Third-party dependencies
+import axios from "axios"
+
+// Current project dependencies
+import httpStatus from "@/constants/common/httpStatus"
+import { IGlobalApiResponse } from "@/ts/interfaces/api"
+import apiRoutes from "@/constants/api/routes"
+
+const getRecentPostsService = async (offset: number, limit: number) => {
+  try {
+    const { data, status } = await axios.get(
+      apiRoutes.post.getRecentPosts(offset, limit)
+    )
+
+    const newResponse: IGlobalApiResponse = {
+      body: data,
+      message: data.message,
+      status: status,
+      success: true,
+    }
+
+    return newResponse
+  } catch (err: unknown) {
+    const apiAnswer = {
+      body: [],
+      message: httpStatus.badRequest.message,
+      success: false,
+      status: httpStatus.badRequest.code,
+    }
+
+    if (axios.isAxiosError(err)) {
+      apiAnswer.body = err.response?.data
+      apiAnswer.message = err.response?.data.message
+      apiAnswer.status = err.response?.status || httpStatus.badRequest.code
+    }
+
+    return apiAnswer
+  }
+}
+
+export default getRecentPostsService
