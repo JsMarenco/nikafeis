19957ab1c0d8137a10d5ecebae422548a0ec747f
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-17 17:42:57 -0600

    Add initial version of getUserConnections controller

---
+++ src/controllers/user/getUserConnections.ts
@@ -0,0 +1,61 @@
+// Third-party dependencies
+import type { NextApiRequest, NextApiResponse } from "next"
+
+// Current project dependencies
+import User from "@/models/User"
+import connectWithRetry from "@/database"
+import httpStatus from "@/constants/common/httpStatus"
+import apiMessages from "@/constants/api/messages"
+
+const getUserConnections = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  try {
+    const { userId = "", offset = "", limit = "" } = req.query
+    const offsetValue = parseInt(offset as string)
+    const limitValue = parseInt(limit as string)
+
+    if (isNaN(offsetValue)) {
+      return res.status(httpStatus.badRequest.code).json({
+        message: apiMessages.errors.common.offsetMustBeNumber,
+      })
+    }
+
+    if (isNaN(limitValue)) {
+      return res.status(httpStatus.badRequest.code).json({
+        message: apiMessages.errors.common.offsetMustBeNumber,
+      })
+    }
+
+    if (!userId) {
+      return res.status(httpStatus.badRequest.code).json({
+        message: apiMessages.errors.common.requiredFields,
+      })
+    }
+
+    await connectWithRetry()
+
+    // TODO: Add the way to get user connections
+
+    const connections = await User.find({}).limit(limitValue).skip(offsetValue)
+
+    res.status(httpStatus.ok.code).json(connections)
+  } catch (error: any) {
+    switch (error.name) {
+      case "CastError":
+        res.status(httpStatus.badRequest.code).send({
+          message: apiMessages.errors.authentication.invalidId,
+        })
+        break
+
+      default:
+        res.status(httpStatus.serverError.code).json({
+          message: httpStatus.serverError.message,
+        })
+        break
+    }
+  }
+}
+
+export default getUserConnections
