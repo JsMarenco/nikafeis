2cf912b016e6a205c966bebe4eb578a6c09d8f0f
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-17 15:05:59 -0600

    Add endpoint for deleting users

--- src/pages/api/users/index.ts
+++ src/pages/api/users/index.ts
@@ -4,11 +4,16 @@ import type { NextApiRequest, NextApiResponse } from "next"
 // Current project dependencies
 import httpStatus from "@/constants/common/httpStatus"
 import registerUser from "@/controllers/user/registerUser"
+import deleteUser from "@/controllers/user/deleteUser"
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   switch (req.method) {
     case "POST":
       registerUser(req, res)
+      break
+
+    case "DELETE":
+      deleteUser(req, res)
       break
 
     default:
