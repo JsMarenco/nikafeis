cec6f7578626b208de10eee96629433eef5971eb
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-17 18:31:18 -0600

    Add index to export themes

---
+++ src/themes/index.ts
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import darkTheme from "./dark"
+import lightTheme from "./light"
+
+/**
+ * All theme app themes names
+ */
+enum appThemes {
+  dark = "dark_theme",
+  light = "light_theme",
+}
+
+export { appThemes, darkTheme, lightTheme }
