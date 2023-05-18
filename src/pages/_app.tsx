56de1dc3d3d400cc358e090717bc95c7da3df304
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-18 11:18:43 -0600

    Add font to _app.tsx and minor changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,12 @@
-import "@/styles/globals.css"
+// Third-party dependenciesl
 import type { AppProps } from "next/app"
+import "@fontsource/roboto/300.css"
+import "@fontsource/roboto/400.css"
+import "@fontsource/roboto/500.css"
+import "@fontsource/roboto/700.css"
+
+// Current project dependenciesl
+import "@/styles/globals.css"
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
