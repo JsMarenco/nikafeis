4eae5577e07b2ead13943546bebbe168f79fb883
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-18 14:14:50 -0600

    Add appTitles for page names

---
+++ src/ts/enums/appTitles.ts
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+
+enum appTitles {
+  home = "Home",
+  about = "About",
+  contact = "Contact",
+  faq = "FAQ",
+  termsAndConditions = "Terms and Conditions",
+  privacyPolicy = "Privacy Policy",
+  login = "Login",
+  register = "Register",
+}
+
+export default appTitles
