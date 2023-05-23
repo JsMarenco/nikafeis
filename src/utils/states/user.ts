c654da0e4c984f71b057c5f5f24ac6a35e312893
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-23 13:07:12 -0600

    Add initial user state

---
+++ src/utils/states/user.ts
@@ -0,0 +1,51 @@
+// Third-party dependencies
+
+// Current project dependencies
+import { IInitialUserState } from "@/ts/interfaces/states/states"
+
+const initialUserState: IInitialUserState = {
+  auth: {
+    accessToken: "",
+  },
+
+  personalInfo: {
+    firstname: "",
+    lastname: "",
+    description: "",
+    avatarUrl: "",
+    coverUrl: "",
+    website: "",
+  },
+
+  accountInfo: {
+    id: "",
+    username: "",
+    email: "",
+    password: "",
+  },
+
+  socialMediaLinks: {
+    facebookLink: "",
+    githubLink: "",
+    linkedinLink: "",
+    twitterLink: "",
+    instagramLink: "",
+  },
+
+  profileInfo: {
+    posts: [],
+  },
+
+  timestamps: {
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+
+  relationships: {
+    friends: [],
+    friendRequests: [],
+    friendRequestsSent: [],
+  },
+}
+
+export default initialUserState
