b818914b25f67a09cb68b3eab77d8e0011ecfb10
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-23 00:18:31 -0600

    Add IContextChildrenProps interface

---
+++ src/ts/interfaces/context.ts
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import { ReactNode } from "react"
+
+// Third-party dependencies
+
+// Current project dependencies
+
+/**
+ * Context Children Props Interface
+ */
+export interface IContextChildrenProps {
+  children: ReactNode
+}
