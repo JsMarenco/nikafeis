ad64e66857b71b8c496a7f4842a4a5c01e9ab0a9
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-23 17:29:10 -0600

    Add CustomMenuItemProps interface

---
+++ src/ts/interfaces/menu.ts
@@ -0,0 +1,11 @@
+import { ReactNode } from "react"
+
+// Third-party dependencies
+
+// Current project dependencies
+
+export interface CustomMenuItemProps {
+  icon: ReactNode
+  label: string
+  link: string
+}
