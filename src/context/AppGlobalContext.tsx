e3e75b90ebd9ca05fc149c91bf7ba1e399b0ea1f
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-23 00:31:33 -0600

    Add AppGlobalContext

---
+++ src/context/AppGlobalContext.tsx
@@ -0,0 +1,13 @@
+// Third-party dependencies
+
+// Current project dependencies
+import { IContextChildrenProps } from "@/ts/interfaces/context"
+
+/**
+ * App Global context, wrap all the contexts provider
+ * @param props React Node
+ * @returns All the contexts
+ */
+export const AppGlobalContext = (props: IContextChildrenProps) => {
+  return <> </>
+}
