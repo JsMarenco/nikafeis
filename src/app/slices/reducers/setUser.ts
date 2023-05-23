3f1618311b500926247c52beb906f24990d8b238
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-23 13:20:27 -0600

    Add setUser reducer

---
+++ src/app/slices/reducers/setUser.ts
@@ -0,0 +1,23 @@
+// Third-party dependencies
+import { CaseReducer, PayloadAction } from "@reduxjs/toolkit"
+
+// Current project dependencies
+import { IInitialUserState } from "@/ts/interfaces/states/states"
+import { IUser } from "@/ts/interfaces/user"
+
+const setUserReducer: CaseReducer<IInitialUserState, PayloadAction<IUser>> = (
+  state,
+  action
+) => {
+  const { ...userProps } = action.payload
+
+  state.auth = { ...userProps }
+  state.personalInfo = { ...userProps }
+  state.accountInfo = { ...userProps }
+  state.socialMediaLinks = { ...userProps }
+  state.profileInfo = { ...userProps }
+  state.timestamps = { ...userProps }
+  state.relationships = { ...userProps }
+}
+
+export default setUserReducer
