457744847a227b95c5a7cd05d19d7ca587fdcd81
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-23 13:21:02 -0600

    Add setUser reducer to userSlice

--- src/app/slices/user.ts
+++ src/app/slices/user.ts
@@ -4,13 +4,18 @@ import { createSlice } from "@reduxjs/toolkit"
 // Current project dependencies
 import initialUserState from "@/utils/states/user"
 import { IInitialUserState } from "@/ts/interfaces/states/states"
+import setUserReducer from "./reducers/setUser"
 
 const initialUser: IInitialUserState = initialUserState
 
 const userSlice = createSlice({
   name: "user",
   initialState: initialUser,
-  reducers: {},
+  reducers: {
+    setUser: setUserReducer,
+  },
 })
+
+export const { setUser } = userSlice.actions
 
 export default userSlice.reducer
