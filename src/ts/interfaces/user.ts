13b9a39678a127340777a42f771eb05100351b4e
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-23 11:32:14 -0600

    Refactor IUser interface

--- src/ts/interfaces/user.ts
+++ src/ts/interfaces/user.ts
@@ -1,34 +1,23 @@
 // Third-party dependencies
-import { Schema } from "mongoose"
 
 // Current project dependencies
+import { IAccountInfo } from "./user/accountInfo"
+import { IPersonalInfo } from "./user/personalInfo"
+import { IProfileInfo } from "./user/profileInfo"
+import { IRelationships } from "./user/relationships"
+import { ISocialMediaLinks } from "./user/socialMediaLinks"
+import { ITimestamps } from "./user/timestamps"
 
 /**
  * Interface representing an user.
  */
-export interface IUser {
-  id?: string
-  username: string
-  email: string
-  password: string
-  firstname: string
-  lastname: string
-  description?: string
-  avatarUrl?: string
-  coverUrl?: string
-  website?: string
-  posts: Schema.Types.ObjectId[]
-  friends: Schema.Types.ObjectId[]
-  friendRequests: Schema.Types.ObjectId[]
-  friendRequestsSent: Schema.Types.ObjectId[]
-  facebookLink?: string
-  githubLink?: string
-  linkedinLink?: string
-  twitterLink?: string
-  instagramLink?: string
-  createdAt: Date
-  updatedAt: Date
-}
+export interface IUser
+  extends IAccountInfo,
+    IPersonalInfo,
+    IProfileInfo,
+    IRelationships,
+    ISocialMediaLinks,
+    ITimestamps {}
 
 /**
  * User projection for populating fields.
