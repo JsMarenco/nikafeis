dd9d7c99b2bd9bf11f076ec95339ce9c4906a81e
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-23 13:57:57 -0600

    Add store provider to AppGlobalContext

--- src/context/AppGlobalContext.tsx
+++ src/context/AppGlobalContext.tsx
@@ -1,10 +1,12 @@
 // Third-party dependencies
 import { SnackbarProvider } from "notistack"
+import { Provider } from "react-redux"
 
 // Current project dependencies
 import { AppMessageContextProvider } from "./AppMessageContext"
 import { IContextChildrenProps } from "@/ts/interfaces/context"
 import { AppThemeContextProvider } from "./AppThemeContext"
+import { store } from "@/app/store"
 
 /**
  * App Global context, wrap all the contexts provider
@@ -14,11 +16,13 @@ import { AppThemeContextProvider } from "./AppThemeContext"
 export const AppGlobalContext = (props: IContextChildrenProps) => {
   return (
     <>
-      <SnackbarProvider maxSnack={3} style={{ backgroundColor: "black" }}>
-        <AppMessageContextProvider>
-          <AppThemeContextProvider>{props.children}</AppThemeContextProvider>
-        </AppMessageContextProvider>
-      </SnackbarProvider>
+      <Provider store={store}>
+        <SnackbarProvider maxSnack={3} style={{ backgroundColor: "black" }}>
+          <AppMessageContextProvider>
+            <AppThemeContextProvider>{props.children}</AppThemeContextProvider>
+          </AppMessageContextProvider>
+        </SnackbarProvider>
+      </Provider>
     </>
   )
 }
