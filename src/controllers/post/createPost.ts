db387e243bf677232edf3c6d4878f6ed21aedf2e
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-24 21:51:58 -0600

    Update: Added database connection and additional validations

--- src/controllers/post/createPost.ts
+++ src/controllers/post/createPost.ts
@@ -1,24 +1,29 @@
 // Third-party dependencies
-import type { NextApiRequest, NextApiResponse } from "next"
-import { HydratedDocument } from "mongoose"
+import mongoose, { HydratedDocument } from "mongoose"
+import { NextApiRequest, NextApiResponse } from "next"
 
 // Current project dependencies
 import httpStatus from "@/constants/common/httpStatus"
 import apiMessages from "@/constants/api/messages"
 import User from "@/models/User"
 import { IUser } from "@/ts/interfaces/user"
-import { IPost } from "@/ts/interfaces/post"
+import { ICreatePost, IPost } from "@/ts/interfaces/post"
 import Post from "@/models/Post"
+import connectWithRetry from "@/database"
 
 const createPost = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    const { userId = "", title = "", content = "" } = req.body
+    const data = JSON.parse(JSON.stringify(req.body))
+    const { title = "", content = "" }: ICreatePost = data
+    const { userId = "" } = req.query
 
-    if (!userId || (!title && !content)) {
+    if (!userId || !title || !content) {
       return res.status(httpStatus.badRequest.code).json({
         message: apiMessages.errors.common.requiredFields,
       })
     }
+
+    await connectWithRetry()
 
     const user: HydratedDocument<IUser> | null = await User.findById(userId)
 
@@ -28,13 +33,12 @@ const createPost = async (req: NextApiRequest, res: NextApiResponse) => {
       })
     }
 
-    // TODO: Add token validation
     // TODO: Add cloudianry
 
     const newPost: HydratedDocument<IPost> = new Post({
       title,
       content,
-      postImages: "",
+      postImages: [],
       author: user.id,
     })
 
@@ -49,6 +53,7 @@ const createPost = async (req: NextApiRequest, res: NextApiResponse) => {
       message: apiMessages.success.post.created,
     })
   } catch (error: any) {
+    console.log("🚀 ~ file: createPost.ts:57 ~ createPost ~ error:", error)
     switch (error.name) {
       case "CastError":
         res.status(httpStatus.badRequest.code).send({
@@ -62,6 +67,8 @@ const createPost = async (req: NextApiRequest, res: NextApiResponse) => {
         })
         break
     }
+  } finally {
+    mongoose.disconnect()
   }
 }
 
