ed929e7bd15243567ef59b1437f714cacef7941b
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-25 17:10:26 -0600

    Update: Added database connection, additional validations, and fixed bugs

--- src/controllers/post/getRecentPosts.ts
+++ src/controllers/post/getRecentPosts.ts
@@ -1,15 +1,23 @@
 // Third-party dependencies
 import type { NextApiRequest, NextApiResponse } from "next"
+import mongoose from "mongoose"
 
 // Current project dependencies
 import httpStatus from "@/constants/common/httpStatus"
 import apiMessages from "@/constants/api/messages"
 import { userProjection } from "@/ts/interfaces/user"
 import Post from "@/models/Post"
+import connectWithRetry from "@/database"
 
 const getRecentPosts = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const { offset = "", limit = "" } = req.query
+
+    if (offset === "" || limit === "") {
+      return res.status(httpStatus.badRequest.code).json({
+        message: apiMessages.errors.common.limitOffset,
+      })
+    }
 
     const offsetValue = parseInt(offset as string)
     const limitValue = parseInt(limit as string)
@@ -22,9 +30,11 @@ const getRecentPosts = async (req: NextApiRequest, res: NextApiResponse) => {
 
     if (isNaN(limitValue)) {
       return res.status(httpStatus.badRequest.code).json({
-        message: apiMessages.errors.common.offsetMustBeNumber,
+        message: apiMessages.errors.common.limitMustBeNumber,
       })
     }
+
+    await connectWithRetry()
 
     const posts = await Post.find()
       .populate({
@@ -42,11 +52,17 @@ const getRecentPosts = async (req: NextApiRequest, res: NextApiResponse) => {
       .limit(limitValue)
       .skip(offsetValue)
 
-    res.status(httpStatus.ok.code).json(posts)
+    res.status(httpStatus.ok.code).json(posts.reverse())
   } catch (error) {
+    console.log(
+      "🚀 ~ file: getRecentPosts.ts:56 ~ getRecentPosts ~ error:",
+      error
+    )
     res.status(httpStatus.serverError.code).json({
       message: httpStatus.serverError.message,
     })
+  } finally {
+    mongoose.disconnect()
   }
 }
 
