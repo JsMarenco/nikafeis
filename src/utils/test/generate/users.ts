2664a913460c2de0d9a5c67afd38e02aff86aa7b
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-25 17:13:31 -0600

    New Feature: Added functions generateFakeUser, generateFakeUsers, and uploadFakeUsersToDB

---
+++ src/utils/test/generate/users.ts
@@ -0,0 +1,74 @@
+// Third-party dependencies
+import { faker } from "@faker-js/faker"
+
+// Current project dependencies
+import User from "@/models/User"
+import { IRegisterUser, IUser } from "@/ts/interfaces/user"
+import connectWithRetry from "@/database"
+import { generateUsername } from "@/utils/basic"
+
+export const generateFakeUser = (): IRegisterUser => {
+  const email = `${faker.person.fullName()}_${faker.number.int({
+    min: 1,
+    max: 100,
+  })}@gmail.com`
+    .toLocaleLowerCase()
+    .replace(" ", "")
+
+  const password = faker.lorem.sentence({ min: 2, max: 5 })
+
+  return {
+    firstname: faker.person.firstName("male"),
+    lastname: faker.person.lastName("male"),
+    email,
+    password,
+    confirmPassword: password,
+  }
+}
+
+export const generateFakeUsers = (limit: number): IRegisterUser[] => {
+  const users: IRegisterUser[] = []
+
+  for (let i = 0; i < limit; i++) {
+    const user = generateFakeUser()
+
+    users.push(user)
+  }
+
+  return users
+}
+
+export const uploadFakeUsersToDB = async (
+  users: IRegisterUser[]
+): Promise<IUser[]> => {
+  try {
+    await connectWithRetry()
+
+    await User.deleteMany({})
+
+    interface userWithUsername extends IRegisterUser {
+      username: string
+    }
+
+    const usersWithUsername: userWithUsername[] = []
+
+    for (let i = 0; i < users.length; i++) {
+      const userWithUsername: userWithUsername = {
+        ...users[i],
+        username: generateUsername(),
+      }
+
+      usersWithUsername.push(userWithUsername)
+    }
+
+    await User.insertMany(usersWithUsername)
+
+    const usersDB = await User.find({})
+
+    return usersDB
+  } catch (error) {
+    console.log("🚀 ~ file: users.ts:71 ~ error:", error)
+  }
+
+  return []
+}
