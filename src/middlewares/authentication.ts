d54975d536b6b1696c8d450c0f487e85f13b3b97
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-24 23:57:18 -0600

    Update: Changed authentication header name to Authorization

--- src/middlewares/authentication.ts
+++ src/middlewares/authentication.ts
@@ -16,7 +16,10 @@ const authentication = async (
   handler: NextApiHandler
 ) => {
   try {
-    const token = req.headers.authorization?.split(" ")[1]
+    const data = JSON.parse(JSON.stringify(req.headers))
+    const { Authorization = "" } = data
+
+    const token: string = Authorization.split(" ")[1]
 
     if (!token) {
       return res.status(httpStatus.badRequest.code).json({
