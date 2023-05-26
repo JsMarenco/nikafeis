4b444c09e5734aab7533b4af20e4d151edd8e47e
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-26 16:57:19 -0600

    New Feature: Added new routes

--- src/constants/api/routes/comments.ts
+++ src/constants/api/routes/comments.ts
@@ -1,13 +1,17 @@
 const baseUrl = "/api/comments"
 
 const apiCommentsRoutes = {
-  create: `${baseUrl}`,
+  create: (postId: string, userId: string) =>
+    `${baseUrl}?postId=${postId}&userId=${userId}`,
 
   update: (commentId: string, userId: string) =>
     `${baseUrl}/${commentId}?userId=${userId}`,
 
   like: (commentId: string, userId: string) =>
     `${baseUrl}/${commentId}/like?userId=${userId}`,
+
+  report: (commentId: string, userId: string) =>
+    `${baseUrl}/${commentId}/report?userId=${userId}`,
 
   delete: (commentId: string, userId: string) =>
     `${baseUrl}/${commentId}?userId=${userId}`,
