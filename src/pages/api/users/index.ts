ccd2186e93c87e2a14770fc768aa7e2f7ac39a00
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-26 03:06:24 -0600

    Update: Changed handler name

--- src/pages/api/users/index.ts
+++ src/pages/api/users/index.ts
@@ -1,12 +1,15 @@
 // Third-party dependencies
-import type { NextApiRequest, NextApiResponse } from "next"
+import { NextApiRequest, NextApiResponse } from "next"
 
 // Current project dependencies
 import httpStatus from "@/constants/common/httpStatus"
 import registerUser from "@/controllers/user/registerUser"
 import deleteUser from "@/controllers/user/deleteUser"
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default function registerHandler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   switch (req.method) {
     case "POST":
       registerUser(req, res)
