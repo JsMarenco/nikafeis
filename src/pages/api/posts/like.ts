c671500c4465694047e20976cb088548436afef4
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-26 13:13:59 -0600

    Update: Changed handler name and added authentication middleware

--- src/pages/api/posts/like.ts
+++ src/pages/api/posts/like.ts
@@ -4,11 +4,12 @@ import type { NextApiRequest, NextApiResponse } from "next"
 // Current project dependencies
 import httpStatus from "@/constants/common/httpStatus"
 import likePost from "@/controllers/post/likePost"
+import authentication from "@/middlewares/authentication"
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default function likeHandler(req: NextApiRequest, res: NextApiResponse) {
   switch (req.method) {
     case "PUT":
-      likePost(req, res)
+      authentication(req, res, likePost)
       break
 
     default:
