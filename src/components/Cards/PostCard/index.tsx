9baa88adcb4210e01b263c9c88db6f341bb3be6b
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-26 15:09:35 -0600

    New Feature: Implemented handleLike function in PostCard

--- src/components/Cards/PostCard/index.tsx
+++ src/components/Cards/PostCard/index.tsx
@@ -1,16 +1,33 @@
+import { useContext, useState } from "react"
+
 // Third-party dependencies
+import { useSelector } from "react-redux"
 
 // Current project dependencies
-import { useState } from "react"
 import PostCardUI from "./PostCardUI"
 import { IPostWithPopulated } from "@/ts/interfaces/post"
+import likePostService from "@/services/post/likePostService"
+import { RootState } from "@/app/store"
+import { AppMessageContext } from "@/context/AppMessageContext"
 
 export default function PostCard(postProps: IPostWithPopulated) {
   // eslint-disable-next-line no-unused-vars
   const [post, setPost] = useState<IPostWithPopulated>(postProps)
+  const { accountInfo, auth } = useSelector((state: RootState) => state.user)
+  const { handleMessage } = useContext(AppMessageContext)
 
   const handleLike = async () => {
-    console.log("Liked")
+    const { body, message, success } = await likePostService(
+      post.id,
+      accountInfo.id,
+      auth.accessToken
+    )
+
+    if (success) {
+      setPost((prevPost) => ({ ...prevPost, likes: body.likes }))
+    }
+
+    handleMessage(message)
   }
 
   const handleOpenCommentsMenu = async () => {
@@ -24,6 +41,7 @@ export default function PostCard(postProps: IPostWithPopulated) {
   return (
     <>
       <PostCardUI
+        id={post.id}
         author={post.author}
         comments={post.comments}
         title={post.title}
