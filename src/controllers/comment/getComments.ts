d04899ae6c7701a8391dbefaed0c6199743bb8a8
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-27 01:29:19 -0600

    Bug Fix: Fixed validation errors, added finally block to close the database connection

--- src/controllers/comment/getComments.ts
+++ src/controllers/comment/getComments.ts
@@ -1,6 +1,6 @@
 // Third-party dependencies
 import type { NextApiRequest, NextApiResponse } from "next"
-import { HydratedDocument } from "mongoose"
+import mongoose, { HydratedDocument } from "mongoose"
 
 // Current project dependencies
 import httpStatus from "@/constants/common/httpStatus"
@@ -15,24 +15,30 @@ const getComments = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const { postId = "", limit = "", offset = "" } = req.query
 
-    const offsetValue = parseInt(offset as string)
-    const limitValue = parseInt(limit as string)
-
-    if (isNaN(offsetValue)) {
-      return res.status(httpStatus.badRequest.code).json({
-        message: apiMessages.errors.common.offsetMustBeNumber,
-      })
-    }
-
-    if (isNaN(limitValue)) {
-      return res.status(httpStatus.badRequest.code).json({
-        message: apiMessages.errors.common.offsetMustBeNumber,
-      })
-    }
-
     if (!postId) {
       return res.status(httpStatus.badRequest.code).json({
         message: apiMessages.errors.common.requiredFields,
+      })
+    }
+
+    if (offset === "" || limit === "") {
+      return res.status(httpStatus.badRequest.code).json({
+        message: apiMessages.errors.common.limitOffset,
+      })
+    }
+
+    const offsetValue = parseInt(offset as string)
+    const limitValue = parseInt(limit as string)
+
+    if (isNaN(offsetValue) || offsetValue < 0) {
+      return res.status(httpStatus.badRequest.code).json({
+        message: apiMessages.errors.common.offsetMustBeNumber,
+      })
+    }
+
+    if (isNaN(limitValue) || limitValue < 0) {
+      return res.status(httpStatus.badRequest.code).json({
+        message: apiMessages.errors.common.limitMustBeNumber,
       })
     }
 
@@ -66,6 +72,8 @@ const getComments = async (req: NextApiRequest, res: NextApiResponse) => {
         })
         break
     }
+  } finally {
+    mongoose.disconnect()
   }
 }
 
