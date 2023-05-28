7f3da760a1a0d6f4c09f61181438bd01b6e71b3b
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-28 05:14:18 -0600

    Update: Renamed handler and added authentication middleware

--- src/pages/api/friend-requests/send.ts
+++ src/pages/api/friend-requests/send.ts
@@ -4,15 +4,19 @@ import type { NextApiRequest, NextApiResponse } from "next"
 // Current project dependencies
 import httpStatus from "@/constants/common/httpStatus"
 import sendFriendRequest from "@/controllers/friendRequest/sendFriendRequest"
+import authentication from "@/middlewares/authentication"
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default function sendFriendRequestHandler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   switch (req.method) {
     case "POST":
-      sendFriendRequest(req, res)
+      authentication(req, res, sendFriendRequest)
       break
 
     default:
-      res.status(httpStatus.methodNotAllowed.code).json({
+      return res.status(httpStatus.methodNotAllowed.code).json({
         message: httpStatus.methodNotAllowed.message,
       })
   }
