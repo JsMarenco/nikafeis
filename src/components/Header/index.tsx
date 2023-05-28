c04f92a435b56b028ccc8c0533d256505ada0555
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-27 18:40:24 -0600

    New Feature: Added theme toggle button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,19 @@
 // Third-party dependencies
-import { AppBar, Stack, Toolbar } from "@mui/material"
+import { AppBar, Stack, Toolbar, IconButton } from "@mui/material"
+import DarkModeIcon from "@mui/icons-material/DarkMode"
+import LightModeIcon from "@mui/icons-material/LightMode"
 
 // Current project dependencies
 import Logo from "../Logo"
+import { useContext } from "react"
+import { AppThemeContext } from "@/context/AppThemeContext"
+import { appThemes } from "@/themes"
 
 export default function Header() {
+  const { handleChangeThemeApp, currentThemeName } = useContext(AppThemeContext)
+
   return (
-    <AppBar position="fixed" color="primary">
+    <AppBar position="sticky" color="primary">
       <Toolbar
         sx={{
           maxWidth: "xl",
@@ -17,6 +24,24 @@ export default function Header() {
       >
         <Stack>
           <Logo />
+        </Stack>
+
+        <Stack
+          direction="row"
+          alignItems="center"
+          justifyContent="end"
+          spacing={0.5}
+        >
+          <IconButton
+            aria-label={`${currentThemeName} button`}
+            onClick={handleChangeThemeApp}
+          >
+            {currentThemeName === appThemes.dark ? (
+              <LightModeIcon />
+            ) : (
+              <DarkModeIcon />
+            )}
+          </IconButton>
         </Stack>
       </Toolbar>
     </AppBar>
