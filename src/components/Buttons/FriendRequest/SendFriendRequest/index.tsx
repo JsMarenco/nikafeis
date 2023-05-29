148fa1ad01cac093b84495b8cc4ff7d80f87321f
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-28 22:51:29 -0600

    New Feature: Implemented handleSendFriendRequest

--- src/components/Buttons/FriendRequest/SendFriendRequest/index.tsx
+++ src/components/Buttons/FriendRequest/SendFriendRequest/index.tsx
@@ -1,14 +1,44 @@
+import { useContext, useState } from "react"
+
 // Third-party dependencies
 import PersonAddIcon from "@mui/icons-material/PersonAdd"
 import { Button, IconButton, useMediaQuery } from "@mui/material"
+import { useDispatch, useSelector } from "react-redux"
 
 // Current project dependencies
+import { AppMessageContext } from "@/context/AppMessageContext"
+import { RootState } from "@/app/store"
+import sendFriendRequestService from "@/services/friendRequest/sendFriendRequestService"
+import { setUserFriendRequestsSent } from "@/app/slices/user"
 
-export default function SendFriendRequest() {
+interface SendFriendRequestProps {
+  receiverId: string
+}
+
+export default function SendFriendRequest({
+  receiverId,
+}: SendFriendRequestProps) {
   const matches = useMediaQuery("(min-width:600px)")
+  const [loading, setLoading] = useState(false)
+  const { handleMessage } = useContext(AppMessageContext)
+  const { accountInfo, auth } = useSelector((state: RootState) => state.user)
+  const dispatch = useDispatch()
 
-  const handleSendFriendRequest = () => {
-    console.log("Added")
+  const handleSendFriendRequest = async () => {
+    setLoading(true)
+
+    const { body, success, message } = await sendFriendRequestService(
+      accountInfo.id,
+      receiverId,
+      auth.accessToken
+    )
+
+    if (success) {
+      dispatch(setUserFriendRequestsSent(body.friendRequestsSent))
+    }
+
+    setLoading(false)
+    handleMessage(message)
   }
 
   return (
@@ -25,6 +55,7 @@ export default function SendFriendRequest() {
             color: "text.primary",
           }}
           disableElevation
+          disabled={loading}
         >
           Add friend
         </Button>
@@ -37,6 +68,7 @@ export default function SendFriendRequest() {
             border: "1px solid",
             borderColor: "text.primary",
           }}
+          disabled={loading}
         >
           <PersonAddIcon />
         </IconButton>
