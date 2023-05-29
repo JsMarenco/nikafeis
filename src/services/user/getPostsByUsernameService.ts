aa24f14e40730dbe90151449c54337c2a62b3479
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-29 07:10:18 -0600

    New Feature: Added service to retrieve user posts by username.

---
+++ src/services/user/getPostsByUsernameService.ts
@@ -0,0 +1,45 @@
+// Third-party dependencies
+import axios from "axios"
+
+// Current project dependencies
+import httpStatus from "@/constants/common/httpStatus"
+import { IGlobalApiResponse } from "@/ts/interfaces/api"
+import apiRoutes from "@/constants/api/routes"
+
+const getPostsByUsernameService = async (
+  username: string,
+  offset: number,
+  limit: number
+) => {
+  try {
+    const { data, status } = await axios.get(
+      apiRoutes.user.getUserPostsByUsername(username, offset, limit)
+    )
+
+    const newResponse: IGlobalApiResponse = {
+      body: data,
+      message: data.message,
+      status: status,
+      success: true,
+    }
+
+    return newResponse
+  } catch (err: unknown) {
+    const apiAnswer = {
+      body: [],
+      message: httpStatus.badRequest.message,
+      success: false,
+      status: httpStatus.badRequest.code,
+    }
+
+    if (axios.isAxiosError(err)) {
+      apiAnswer.body = err.response?.data
+      apiAnswer.message = err.response?.data.message
+      apiAnswer.status = err.response?.status || httpStatus.badRequest.code
+    }
+
+    return apiAnswer
+  }
+}
+
+export default getPostsByUsernameService
