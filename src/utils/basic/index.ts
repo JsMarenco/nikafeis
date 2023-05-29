3e331d5131c40068c47f5ff29dd4f9707a27c4e4
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-28 23:26:03 -0600

    New Feature: Added convertDate function

--- src/utils/basic/index.ts
+++ src/utils/basic/index.ts
@@ -121,3 +121,36 @@ export const decodeJWT = (token: string) => {
  * This animation type will be applied to all skeletons
  */
 export const skeletonAnimation: "pulse" | "wave" = "wave"
+
+/**
+ * Covert the date object to a normal date
+ * @param date Is the raw date object
+ * @returns A normal date
+ */
+export const convertDate = (date: string) => {
+  const dateObject = new Date(date)
+  const now = new Date()
+
+  const diff = now.getTime() - dateObject.getTime()
+
+  const seconds = Math.floor(diff / 1000)
+  const minutes = Math.floor(diff / 1000 / 60)
+  const hours = Math.floor(diff / 1000 / 60 / 60)
+  const days = Math.floor(diff / 1000 / 60 / 60 / 24)
+  const months = Math.floor(diff / 1000 / 60 / 60 / 24 / 30)
+  const years = Math.floor(diff / 1000 / 60 / 60 / 24 / 30 / 12)
+
+  if (seconds < 60) {
+    return `${seconds} seconds ago`
+  } else if (minutes < 60) {
+    return `${minutes} minutes ago`
+  } else if (hours < 24) {
+    return `${hours} hours ago`
+  } else if (days < 30) {
+    return `${days} days ago`
+  } else if (months < 12) {
+    return `${months} months ago`
+  } else {
+    return `${years} years ago`
+  }
+}
