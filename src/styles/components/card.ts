fa0c08ce680a837be7f225379d0bab3b824ba9c8
Angel Marenco <jsmarenco.dev@gmail.com>
2023-05-31 14:05:45 -0600

    New Feature: Added new styles

--- src/styles/components/card.ts
+++ src/styles/components/card.ts
@@ -5,8 +5,24 @@ const container = {
   p: 2,
 }
 
+const button_large = {
+  maxWidth: "165px",
+  minWidth: "120px",
+  borderRadius: 3,
+  py: 1,
+  px: 3,
+  flexShrink: 0,
+}
+
+const button_small = {
+  ...button_large,
+  minWidth: "100px",
+}
+
 const cardStyles = {
   container,
+  button_large,
+  button_small,
 }
 
 export default cardStyles
