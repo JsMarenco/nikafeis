42c216567b182e144672f475ca3051bee44a493a
Angel Marenco <jsmarenco.dev@gmail.com>
2023-06-08 05:26:51 -0600

    Added endpoint to accept friend requests

--- src/pages/api/friend-requests/accept.ts
+++ src/pages/api/friend-requests/accept.ts
@@ -4,11 +4,15 @@ import type { NextApiRequest, NextApiResponse } from "next"
 // Current project dependencies
 import httpStatus from "@/constants/common/httpStatus"
 import acceptFriendRequest from "@/controllers/friendRequest/acceptFriendRequest"
+import authentication from "@/middlewares/authentication"
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default function acceptFriendRequestHandler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   switch (req.method) {
     case "POST":
-      acceptFriendRequest(req, res)
+      authentication(req, res, acceptFriendRequest)
       break
 
     default:
