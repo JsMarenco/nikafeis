100ea7627eb24afe2419dc1effde6ca44bc9739a
Angel Marenco <jsmarenco.dev@gmail.com>
2023-06-08 06:15:23 -0600

    Renamed the handler and added authentication middleware

--- src/pages/api/friend-requests/reject.ts
+++ src/pages/api/friend-requests/reject.ts
@@ -4,11 +4,15 @@ import type { NextApiRequest, NextApiResponse } from "next"
 // Current project dependencies
 import httpStatus from "@/constants/common/httpStatus"
 import rejectFriendRequest from "@/controllers/friendRequest/rejectFriendRequest"
+import authentication from "@/middlewares/authentication"
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default function rejectFriendRequestHandler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   switch (req.method) {
     case "DELETE":
-      rejectFriendRequest(req, res)
+      authentication(req, res, rejectFriendRequest)
       break
 
     default:
