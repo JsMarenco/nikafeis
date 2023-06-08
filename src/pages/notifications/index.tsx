51d453d62f9fe6506d542c928a6ea0f1cedfd6c3
Angel Marenco <jsmarenco.dev@gmail.com>
2023-06-08 05:30:54 -0600

    Added notifications view

---
+++ src/pages/notifications/index.tsx
@@ -0,0 +1,8 @@
+// Third-party dependencies
+
+// Current project dependencies
+import ComingSoon from "@/components/ComingSoon"
+
+export default function Notifications() {
+  return <ComingSoon />
+}
