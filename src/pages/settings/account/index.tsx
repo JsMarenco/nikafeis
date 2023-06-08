bbae9265fa6df4d77f9a6523753ef4161ac75b82
Angel Marenco <jsmarenco.dev@gmail.com>
2023-06-08 05:30:30 -0600

    Added settings view

---
+++ src/pages/settings/account/index.tsx
@@ -0,0 +1,8 @@
+// Third-party dependencies
+
+// Current project dependencies
+import AccountSettingsSection from "@/components/Sections/Settings/AccountSettings"
+
+export default function AccountSettings() {
+  return <AccountSettingsSection />
+}
---
+++ src/pages/settings/index.tsx
@@ -0,0 +1,8 @@
+// Third-party dependencies
+
+// Current project dependencies
+import GeneralSettingsSection from "@/components/Sections/Settings/GeneralSettings"
+
+export default function GeneralSettings() {
+  return <GeneralSettingsSection />
+}
