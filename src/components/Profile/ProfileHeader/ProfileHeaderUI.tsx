e36bc3e5a7da837917f436a9356e5dfa604a004f
Angel Marenco <jsmarenco.dev@gmail.com>
2023-06-08 06:39:23 -0600

    Minor changes

--- src/components/Profile/ProfileHeader/ProfileHeaderUI.tsx
+++ src/components/Profile/ProfileHeader/ProfileHeaderUI.tsx
@@ -178,7 +178,11 @@ export default function ProfileHeaderUI({
 
                   <FriendRequestButton
                     onClick={() =>
-                      handleRejectFriendRequest(friendRequestId, userId)
+                      handleRejectFriendRequest(
+                        friendRequestId,
+                        userId,
+                        fetchUserInfo
+                      )
                     }
                     matches={matches}
                     label={"Reject"}
