29456a9337982dbd94ea3b8e3f9265f1de0b2b65
Angel Marenco <jsmarenco.dev@gmail.com>
2023-06-08 05:25:33 -0600

    Added acceptFriendRequestService

---
+++ src/services/friendRequest/acceptFriendRequestService.ts
@@ -0,0 +1,53 @@
+// Third-party dependencies
+import axios from "axios"
+
+// Current project dependencies
+import httpStatus from "@/constants/common/httpStatus"
+import { IGlobalApiResponse } from "@/ts/interfaces/api"
+import apiRoutes from "@/constants/api/routes"
+
+const acceptFriendRequestService = async (
+  requestId: string,
+  receiverId: string,
+  token: string
+) => {
+  try {
+    const config = {
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    }
+
+    const { data, status } = await axios.post(
+      apiRoutes.friendRequest.accept(requestId, receiverId),
+      {},
+      config
+    )
+
+    const newResponse: IGlobalApiResponse = {
+      body: data,
+      message: data.message,
+      status: status,
+      success: true,
+    }
+
+    return newResponse
+  } catch (err: unknown) {
+    const apiAnswer = {
+      body: [],
+      message: httpStatus.badRequest.message,
+      success: false,
+      status: httpStatus.badRequest.code,
+    }
+
+    if (axios.isAxiosError(err)) {
+      apiAnswer.body = err.response?.data
+      apiAnswer.message = err.response?.data.message
+      apiAnswer.status = err.response?.status || httpStatus.badRequest.code
+    }
+
+    return apiAnswer
+  }
+}
+
+export default acceptFriendRequestService
