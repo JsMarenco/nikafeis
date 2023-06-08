f9e0fcef1ff374b1a1018a279c80237191e65795
Angel Marenco <jsmarenco.dev@gmail.com>
2023-06-08 05:25:44 -0600

    Added styles for the inputs

---
+++ src/styles/components/inputs.ts
@@ -0,0 +1,22 @@
+const container = {
+  bgcolor: "background.default",
+  p: 1.5,
+  borderRadius: 3,
+  maxWidth: "700px",
+}
+
+const rightIcon = {
+  mr: 1.5,
+}
+
+const leftIcon = {
+  ml: 1.5,
+}
+
+const inputStyles = {
+  container,
+  rightIcon,
+  leftIcon,
+}
+
+export default inputStyles
